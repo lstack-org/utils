776ddcd64f8180c7671625a681f42d2538cf27f2
叶士博 <yeshibo@xlauncher.io>
2022-02-17 10:33:10 +0800

    新增 aes加密

---
+++ pkg/encipherment/aes_test.go
@@ -0,0 +1,18 @@
+package encipherment
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAes(t *testing.T) {
+	orig := "EV8mXRBtnPLVxtIhoxEA3vbTGDqUeBDlUUvu4Kds"
+	key := "launcherSoNBPlus"
+	fmt.Println("原文：", orig)
+
+	encryptCode := AesEncrypt(orig, key)
+	fmt.Println("密文：", encryptCode)
+
+	decryptCode := AesDecrypt(encryptCode, key)
+	fmt.Println("解密结果：", decryptCode)
+}
